a5c3baf28f6163093e7e76f433ce2ce745b40d77
Pivopil <myworkplace8@gmail.com>
2016-08-01 16:04:46 +0300

    Role async service has been added

---
+++ rest/src/main/java/io/github/pivopil/rest/services/RoleAsyncService.java
@@ -0,0 +1,30 @@
+package io.github.pivopil.rest.services;
+
+import com.github.pgasync.Db;
+import com.github.pgasync.ResultSet;
+import com.github.pgasync.Row;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import rx.Observable;
+
+/**
+ * Created on 29.07.16.
+ */
+@Service
+public class RoleAsyncService {
+
+    public static final String SELECT_ROLES = "select * from roles";
+
+    @Autowired
+    private Db postgresAsyncConnectionPool;
+
+
+    public Observable<Row> findAllRolesInDBAsRows(){
+        return postgresAsyncConnectionPool.queryRows(SELECT_ROLES);
+    }
+
+    public Observable<ResultSet> findAllRolesInDBAsResultSet(){
+        return postgresAsyncConnectionPool.querySet(SELECT_ROLES);
+    }
+
+}
